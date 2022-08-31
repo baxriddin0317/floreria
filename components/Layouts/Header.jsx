826ff3b3b8ca1fd6aab9df8fcf9c1c5ebe4a683e
AbdushukurRasulov <abdushukurrasulov1994@gmail.com>
2022-08-31 18:47:47 +0500

    update: fonts typo update

--- components/Layouts/Header.jsx
+++ components/Layouts/Header.jsx
@@ -1,187 +1,211 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import { AiOutlineSearch, AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
-import { IoDiamondOutline } from "react-icons/io5"
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { AiOutlineSearch, AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { IoDiamondOutline } from "react-icons/io5";
 
 const links = [
-    {
-        id: 1,
-        text: "novedades",
-        link: "/novedades"
-    },
-    {
-        id: 2,
-        text: "mas vendidos",
-        link: "/mas-vendidos"
-    },
-    {
-        id: 3,
-        text: "cumpleaños",
-        link: "/cumpleanos"
-    },
-    {
-        id: 4,
-        text: "aniversario",
-        link: "/aniversario"
-    },
-    {
-        id: 5,
-        text: "Ofertas",
-        link: "/ofertas"
-    },
-    {
-        id: 6,
-        text: "porque sí",
-        link: "/porqu-si"
-    },
-]
+  {
+    id: 1,
+    text: "novedades",
+    link: "/",
+  },
+  {
+    id: 2,
+    text: "mas vendidos",
+    link: "/",
+  },
+  {
+    id: 3,
+    text: "cumpleaños",
+    link: "/",
+  },
+  {
+    id: 4,
+    text: "aniversario",
+    link: "/",
+  },
+  {
+    id: 5,
+    text: "Ofertas",
+    link: "/",
+  },
+  {
+    id: 6,
+    text: "porque sí",
+    link: "/",
+  },
+];
 function Header() {
-
-    const [ open, setOpen ] = useState(false);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
+  const [open, setOpen] = useState(false);
 
   return (
-    <header className=''>
-        <div className="flex items-center justify-between gap-4 xl:gap-0 max-w-8xl mx-auto px-4 md:px-1 py-30">
-            {/* logo */}
-            <Link href={"/"}>
-                <Image className="cursor-pointer" src={require("../../public/Assets/Icons/logo.png")} width={51} height={57}  />
-            </Link>
+    <header className="">
+      <div className="flex items-center justify-between gap-4 xl:gap-0 max-w-8xl mx-auto px-4 md:px-1 py-30">
+        {/* logo */}
+        <Link href={"/"}>
+          <Image
+            className="cursor-pointer"
+            src={require("../../public/Assets/Icons/logo.png")}
+            width={51}
+            height={57}
+          />
+        </Link>
 
-            {/* form */}
-            <div className='flex items-center '>
-                <form className='md:w-2.5xl ' onSubmit={handleSubmit}>
-                    <div className='flex items-center gap-2 bg-gray-bg px-5 py-3.5 rounded-10 '>
-                        <input className='outline-none bg-transparent w-full text-base' placeholder='¿Qué andas buscando?' type="text" />
-                        <button type='submit'>
-                            <AiOutlineSearch className='text-text-pink text-2xl' />
-                        </button>
-                    </div>
-                </form>
-                <button className='hidden xl:flex gap-4 ml-11 items-center hover:bg-site-pink text-text-pink hover:text-white border border-site-pink rounded-10 py-3.5 px-9'>
-                    <IoDiamondOutline />
-                    <p className='text-base'>Suecia VIP</p>
-                </button>
+        {/* form */}
+        <div className="flex items-center ">
+          <form className="md:w-2.5xl">
+            <div className="flex items-center gap-2 bg-gray-bg px-5 py-3.5 rounded-10 ">
+              <input
+                className="outline-none bg-transparent w-full text-base"
+                placeholder="¿Qué andas buscando?"
+                type="text"
+              />
+              <button type="submit">
+                <AiOutlineSearch className="text-text-pink text-2xl" />
+              </button>
             </div>
-
-            {/* icons */}
-            <ul className="hidden xl:grid grid-cols-3 gap-30">
-             <Link href={"/"}>
-                <li className='flex flex-col items-center justify-between cursor-pointer'>
-                    <Image src={require("../../public/Assets/Icons/car.png")} width={34} height={30} />
-                    <p className='text-text-pink text-xs font-bold '>
-                        Seguimiento
-                    </p>
-                </li>
-             </Link>
-             <Link href="/">
-                <li className='flex flex-col items-center justify-between cursor-pointer'>
-                    <Image src={require("../../public/Assets/Icons/basket.png")} width={34} height={30} />
-                    <p className='text-text-pink text-xs font-bold '>
-                        Carrito
-                    </p>
-                </li>
-             </Link>
-             <Link href="/">
-                <li className='flex flex-col items-center justify-between cursor-pointer'>
-                    <Image src={require("../../public/Assets/Icons/person.png")} width={34} height={30} />
-                    <p className='text-text-pink text-xs font-bold '>
-                        Paloma
-                    </p>
-                </li>
-             </Link>
-            </ul>
-
-            {/* humburger button */}
-            <button 
-                className='flex xl:hidden bg-bg-button p-3 rounded-sm ' 
-                onClick={() => setOpen(!open)}
-            >
-                <AiOutlineMenu className='text-3xl text-text-pink' />
-            </button>
+          </form>
+          <button className="hidden xl:flex gap-4 ml-11 items-center hover:bg-site-pink text-text-pink hover:text-white border border-site-pink rounded-10 py-3.5 px-9">
+            <IoDiamondOutline />
+            <p className="text-base">Suecia VIP</p>
+          </button>
         </div>
 
-        {/* nav */}
-        <div className='hidden xl:block bg-linear pt-0.5 pb-0.5'>
-            <nav className='bg-white  py-30'>
-                <ul className='max-w-5xl mx-auto flex items-center justify-between'>
-                    {links.map(l => (
-                        <li key={l.id} className='text-dark-color hover:text-text-pink text-base font-normal capitalize'>
-                            <Link href={l.link}>
-                                {l.text}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </div>
+        {/* icons */}
+        <ul className="hidden xl:grid grid-cols-3 gap-30">
+          <Link href={"/"}>
+            <li className="flex flex-col items-center justify-between cursor-pointer">
+              <Image
+                src={require("../../public/Assets/Icons/car.png")}
+                width={34}
+                height={30}
+              />
+              <p className="text-text-pink text-xs font-bold ">Seguimiento</p>
+            </li>
+          </Link>
+          <Link href="/">
+            <li className="flex flex-col items-center justify-between cursor-pointer">
+              <Image
+                src={require("../../public/Assets/Icons/basket.png")}
+                width={34}
+                height={30}
+              />
+              <p className="text-text-pink text-xs font-bold ">Carrito</p>
+            </li>
+          </Link>
+          <Link href="/">
+            <li className="flex flex-col items-center justify-between cursor-pointer">
+              <Image
+                src={require("../../public/Assets/Icons/person.png")}
+                width={34}
+                height={30}
+              />
+              <p className="text-text-pink text-xs font-bold ">Paloma</p>
+            </li>
+          </Link>
+        </ul>
 
-        {/* mobile */}
-        <MobileNav open={open} setOpen={setOpen} />
+        {/* humburger button */}
+        <button
+          className="flex xl:hidden bg-bg-button p-3 rounded-sm "
+          onClick={() => setOpen(!open)}
+        >
+          <AiOutlineMenu className="text-3xl text-text-pink" />
+        </button>
+      </div>
+
+      {/* nav */}
+      <div className="hidden xl:block bg-linear pt-0.5 pb-0.5">
+        <nav className="bg-white  py-30">
+          <ul className="max-w-5xl mx-auto flex items-center justify-between">
+            {links.map((l) => (
+              <li
+                key={l.id}
+                className="text-dark-color hover:text-text-pink text-base font-normal capitalize"
+              >
+                <Link href={l.link}>{l.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
+      {/* mobile */}
+      <MobileNav open={open} setOpen={setOpen} />
     </header>
-  )
+  );
 }
 
-function MobileNav ({open, setOpen}) {
-    return (
-        <div className={`bg-white fixed z-100 left-0 w-full h-full top-0 duration-700 ${open ? "left-0" : "-left-full"}`}>
-            <div className='flex items-center justify-between px-4 md:px-1 py-30'>
-                {/* logo */}
-                <Link href={"/"}>
-                    <Image className="cursor-pointer" src={require("../../public/Assets/Icons/logo.png")} width={51} height={57}  />
-                </Link>
+function MobileNav({ open, setOpen }) {
+  return (
+    <div
+      className={`bg-white fixed z-100 left-0 w-full h-full top-0 duration-700 ${
+        open ? "left-0" : "-left-full"
+      }`}
+    >
+      <div className="flex items-center justify-between px-4 md:px-1 py-30">
+        {/* logo */}
+        <Link href={"/"}>
+          <Image
+            className="cursor-pointer"
+            src={require("../../public/Assets/Icons/logo.png")}
+            width={51}
+            height={57}
+          />
+        </Link>
 
-                <button className='p-3' onClick={() => setOpen(false)}>
-                    <AiOutlineClose className='text-3xl text-text-pink' />
-                </button>
-            </div>
-            {/* nav */}
-            <ul className='flex flex-col justify-center items-center gap-y-5 mb-20'>
-                {links.map(l => (
-                    <li 
-                        key={l.id} 
-                        className='text-dark-color hover:text-text-pink text-base font-normal capitalize'
-                        onClick={() => setOpen(false)}
-                    >
-                        <Link href={l.link}>
-                            {l.text}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            {/* icons */}
-            <ul className="flex items-center gap-10 justify-center">
-             <Link href={"/"}>
-                <li className='flex flex-col items-center justify-between cursor-pointer'>
-                    <Image src={require("../../public/Assets/Icons/car.png")} width={34} height={30} />
-                    <p className='text-text-pink text-xs font-bold '>
-                        Seguimiento
-                    </p>
-                </li>
-             </Link>
-             <Link href="/">
-                <li className='flex flex-col items-center justify-between cursor-pointer'>
-                    <Image src={require("../../public/Assets/Icons/basket.png")} width={34} height={30} />
-                    <p className='text-text-pink text-xs font-bold '>
-                        Carrito
-                    </p>
-                </li>
-             </Link>
-             <Link href="/">
-                <li className='flex flex-col items-center justify-between cursor-pointer'>
-                    <Image src={require("../../public/Assets/Icons/person.png")} width={34} height={30} />
-                    <p className='text-text-pink text-xs font-bold '>
-                        Paloma
-                    </p>
-                </li>
-             </Link>
-            </ul>
-        </div>
-    )
+        <button className="p-3" onClick={() => setOpen(false)}>
+          <AiOutlineClose className="text-3xl text-text-pink" />
+        </button>
+      </div>
+      {/* nav */}
+      <ul className="flex flex-col justify-center items-center gap-y-5 mb-20">
+        {links.map((l) => (
+          <li
+            key={l.id}
+            className="text-dark-color hover:text-text-pink text-base font-normal capitalize"
+            onClick={() => setOpen(false)}
+          >
+            <Link href={l.link}>{l.text}</Link>
+          </li>
+        ))}
+      </ul>
+      {/* icons */}
+      <ul className="flex items-center gap-10 justify-center">
+        <Link href={"/"}>
+          <li className="flex flex-col items-center justify-between cursor-pointer">
+            <Image
+              src={require("../../public/Assets/Icons/car.png")}
+              width={34}
+              height={30}
+            />
+            <p className="text-text-pink text-xs font-bold ">Seguimiento</p>
+          </li>
+        </Link>
+        <Link href="/">
+          <li className="flex flex-col items-center justify-between cursor-pointer">
+            <Image
+              src={require("../../public/Assets/Icons/basket.png")}
+              width={34}
+              height={30}
+            />
+            <p className="text-text-pink text-xs font-bold ">Carrito</p>
+          </li>
+        </Link>
+        <Link href="/">
+          <li className="flex flex-col items-center justify-between cursor-pointer">
+            <Image
+              src={require("../../public/Assets/Icons/person.png")}
+              width={34}
+              height={30}
+            />
+            <p className="text-text-pink text-xs font-bold ">Paloma</p>
+          </li>
+        </Link>
+      </ul>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,25 @@ module.exports = {
     extend: {
       maxWidth: {
         "7.5xl": "1400px",
-        "8xl": "1448px"
+        "8xl": "1448px",
       },
       width: {
-        "2.5xl": "645px"
+        "2.5xl": "645px",
       },
       spacing: {
-        "30": "30px",
-        "60": "60px"
+        30: "30px",
+        60: "60px",
       },
       borderRadius: {
-        "70": "80px",
-        "40": "40px",
-        "30": "30px",
-        "20": "20px",
-        "10": "10px"
+        70: "80px",
+        40: "40px",
+        30: "30px",
+        20: "20px",
+        10: "10px",
       },
       backgroundImage: {
-        "linear": "linear-gradient(90deg, rgba(155, 171, 191, 0) 1.04%, #9BABBF 51.56%, rgba(155, 171, 191, 0) 100%)"
+        linear:
+          "linear-gradient(90deg, rgba(155, 171, 191, 0) 1.04%, #9BABBF 51.56%, rgba(155, 171, 191, 0) 100%)",
       },
       colors: {
         "gray-bg": "#F8F8F8",
@@ -34,21 +35,21 @@ module.exports = {
         "dark-color": "#444",
         "gray-text": "#9BABBF",
         "gary-sm": "#818181",
-        "label": "#C8CED4",
+        label: "#C8CED4",
       },
       fontFamily: {
-        "nunito": "Nunito",
-        "Cormorant-Upright": "Cormorant Upright",
-        "Poppins": "Poppins"
+        nunito: ["Nunito", "sans-serif"],
+        "cormorant-upright": ["Cormorant Upright", "serif"],
+        poppins: ["Poppins", "sans-serif"],
       },
       fontSize: {
         "title-xl": ["50px", "61px"],
-        "lg-text": ["22px", "33px"]
+        "lg-text": ["22px", "33px"],
       },
       zIndex: {
-        "100": "100"
-      }
+        100: "100",
+      },
     },
   },
   plugins: [],
-}
+};
