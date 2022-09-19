4df56ab6342947ca305865fea68d09966f9cb202
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2022-09-19 13:35:46 +0500

    letter case

--- components/Layouts/AppFooter.jsx
+++ components/Layouts/AppFooter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Newsletter from "../Sections/Newsletter";
+import Newsletter from "../sections/Newsletter";
 
 const footerLink = [
   {
